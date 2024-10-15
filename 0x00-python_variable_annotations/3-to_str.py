b18450301f1d18c700ff462b8f20c28064c17c7d
Gendozzzz <sunflowermaina@gmail.com>
2024-10-16 02:07:07 +0300

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Returns the string representation of a float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    function to_str that takes
+    float n as argument
+    returns the string representation of the float
+    """
+    return str(n)
